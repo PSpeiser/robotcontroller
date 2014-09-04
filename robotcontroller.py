62ffe1b1742a371e193c4ebfef9cae8d9cb703a9
PSpeiser <mail@patrickspeiser.at>
2014-09-04 10:47:38 +0200

    PiCamera added

--- robotcontroller.py
+++ robotcontroller.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, send_file
 import io
-import os
-#import picamera
+import picamera
 app = Flask(__name__)
 teensy = None
 camera = None
@@ -10,31 +9,12 @@ camera = None
 def hello_world():
     return render_template('robotcontroller.html')
 
-def get_image_bytes():
-    try:
-        b = io.BytesIO()
-        with open('C:/Users/Doskir/Dropbox/Projects/robotcontroller/static/cam.jpg', "rb") as f:
-            while True:
-                data = f.read(1024)
-                if not data:
-                    break
-                b.write(data)
-    except Exception as e:
-        print e
-    b.seek(0)
-    return b
 
 @app.route('/cam.jpg')
 def cam_jpg():
-    try:
-        img = get_image_bytes()
-        return send_file(img, attachment_filename='cam.jpg', mimetype='image/jpeg')
-    except Exception as e:
-        print e
-    return img
-
-    camera.capture()
-    return app.send_static_file('cam.jpg')
+    img = io.BytesIO()
+    camera.capture(img, 'jpeg')
+    return send_file(img, attachment_filename='cam.jpg', mimetype='image/jpeg')
 
 
 @app.route('/motor_vector', methods=["POST"])
@@ -45,6 +25,7 @@ def motor_vector():
     motor_powers = calculate_motor_powers(vector)
     teensy.send_motor_powers(motor_powers)
     return "Success"
+
 
 class Teensy():
     def __init__(self):
@@ -67,6 +48,7 @@ class Teensy():
         for key, value in motor_powers.items():
             self.serial.write("{0}{1}\r\n".format(key, value))
 
+
 def calculate_motor_powers(vector):
     motor_powers = {'FL': round(max(-1.0, min(1.0, vector[1] + vector[0])), 2),
                     'FR': round(max(-1.0, min(1.0, vector[1] - vector[0])), 2),
@@ -76,7 +58,7 @@ def calculate_motor_powers(vector):
 
 if __name__ == '__main__':
     teensy = Teensy()
-    #camera = picamera.PiCamera()
-    #camera.resolution = (640, 480)
-    #camera.rotation = 180
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
+    camera.rotation = 180
     app.run()
